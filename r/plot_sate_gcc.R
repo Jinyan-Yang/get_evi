cf338e25144f999df5b25e31b3cf0a6fa6ee4189
JinyanYang <jyyoung@live.com>
2020-12-08 12:08:08 +1100

    updated plots with rainfall

--- r/plot_sate_gcc.R
+++ r/plot_sate_gcc.R
@@ -10,21 +10,20 @@ ndvi.df.long$NDVI.val <- as.numeric(gsub(',','',as.character(ndvi.df.long$ndvi))
 fapar.df.cw <- read.csv('downloads/fapar_cw.csv')
 fapar.df.cw$Date <- as.Date(fapar.df.cw$system.time_start,'%b %d, %Y')
 
-library(tidyr)
+# 
 fapar.df.cw.long <-pivot_longer(fapar.df.cw,MGAR:IN17,'Code',values_to = 'fapar')
 fapar.df.cw.long$fapar.val <- as.numeric(gsub(',','',as.character(fapar.df.cw.long$fapar))) * 0.01
 # 
 fapar.df.dn <- read.csv('downloads/fapar_dn.csv')
 fapar.df.dn$Date <- as.Date(fapar.df.dn$system.time_start,'%b %d, %Y')
 
-library(tidyr)
 fapar.df.dn.long <-pivot_longer(fapar.df.dn,NG:YM,'Code',values_to = 'fapar')
 fapar.df.dn.long$fapar.val <- as.numeric(gsub(',','',as.character(fapar.df.dn.long$fapar))) * 0.01
 
 fapar.df.long <- rbind(fapar.df.dn.long,fapar.df.cw.long)
 
 #get modis fractional cover  
-watson.sites <- read.csv('watson_site.csv')
+watson.sites <- read.csv('watson_site_new.csv')
 sites.vec <- c('NG','MP','QP','YM',as.character(watson.sites$code))
 
 out.ls <- list()
@@ -65,9 +64,12 @@ cw.gcc.df.cln <- summaryBy(GCCmean~Date + Location + Code + PS + Native,
 gcc.fapar.fc.ndvi.df <- Reduce(function(x,y) merge(x = x, y = y, all=T), 
        list(fapar.df.long, fc.df, ndvi.df.long,cw.gcc.df.cln))
 
+saveRDS(gcc.fapar.fc.ndvi.df,'gcc.fc.ndvi.fapar.rds')
 
 
+# 
 png('pair.gcc.fc.ndvi.fapar.png',width = 800,height = 800 )
+
 pairs(gcc.fapar.fc.ndvi.df[,c('FC','GCCmean','NDVI.val','fapar.val')],upper.panel=NULL,
       panel=function(x,y){
         points(x,y)
@@ -75,4 +77,78 @@ pairs(gcc.fapar.fc.ndvi.df[,c('FC','GCCmean','NDVI.val','fapar.val')],upper.pane
         text(x=quantile(x,probs = 0.99,na.rm = T),y=quantile(y,probs = 0.99,na.rm = T),
              labels = paste('R=',round((cor(x,y,use ='na.or.complete')),2)),col='red' )
       })
+dev.off()
+
+# get a pallete
+devtools::source_url("https://github.com/Jinyan-Yang/colors/blob/master/R/col.R?raw=TRUE")
+# 
+
+palette(col.df$auLandscape)
+pdf('time Series And Pairs By Site.pdf',width = 8,height = 8*.618)
+par(mar=c(5,5,1,5))
+gcc.met.cw.df <- readRDS('cache/gcc.met.cw.df.rds')
+site.vec <- unique(gcc.met.cw.df$Code)
+site.vec <- as.character(site.vec)
+
+for (i in seq_along(site.vec)) {
+  cw.sat.gcc.df <- gcc.fapar.fc.ndvi.df[gcc.fapar.fc.ndvi.df$Code == site.vec[i],]
+  
+  
+  plot(GCCmean~Date,data = cw.sat.gcc.df,type='b',pch=16,col=2,#'blue',
+       xlab='',ylab='Greenness',xlim=c(as.Date('2014-1-1'),
+                                       as.Date('2016-1-1')),
+       ylim=c(0.28,0.45)
+       )
+  title(site.vec[i])
+  legend('topleft',legend = c('GCC','Fractional cover','NDVI','FAPAR','VWC'),
+         pch=16,col=c(2,1,3,4,5))#col=c('red','coral','orange','blue'))
+  if(sum(cw.sat.gcc.df$FC,na.rm=T)>0){
+    par(new=T)
+    plot(FC~Date,data = cw.sat.gcc.df,type='p',pch=16,col=1,#'red',
+         ann=F,axes=F,xlim=c(as.Date('2014-1-1'),
+                             as.Date('2016-1-1')))
+  }
+  if(sum(cw.sat.gcc.df$NDVI.val,na.rm=T)>0){
+    par(new=T)
+    plot(NDVI.val~Date,data = cw.sat.gcc.df,type='p',pch=16,col=3,#'coral',
+             ann=F,axes=F,xlim=c(as.Date('2014-1-1'),
+                                 as.Date('2016-1-1')))
+  }
+  
+  if(sum(cw.sat.gcc.df$fapar.val,na.rm=T)>0){
+    par(new=T)
+    plot(fapar.val~Date,data = cw.sat.gcc.df,type='p',pch=16,col=4,#'orange',
+         ann=F,axes=F,xlim=c(as.Date('2014-1-1'),
+                             as.Date('2016-1-1')))
+  }
+
+  # rainfall
+  fn.site <- paste0('cache/met.',site.vec[i],'20012016.rds')
+  site.rain.df <- readRDS(fn.site)
+  
+  par(new=T)
+  plot(Rain~Date,data = site.rain.df,type='s',col=5,#'lightskyblue',
+       ann=F,axes=F,xlim=c(as.Date('2014-1-1'),
+                           as.Date('2016-1-1')))
+  
+  y.max <- ceiling(max(site.rain.df$Rain,na.rm = T)/10)*10
+  axis(4,at = seq(0,y.max,length.out = 5),labels = seq(0,y.max,length.out = 5))
+  mtext('Rainfall (mm/d)',side =4, line =3)
+  
+  try(pairs(cw.sat.gcc.df[,c('FC','GCCmean','NDVI.val','fapar.val')],upper.panel=NULL,
+        panel=function(x,y){
+          if(sum(x,na.rm=T)>0 & sum(y,na.rm=T)>0){
+            points(x,y)
+            abline(lm(y~x), col='red',lwd=2,lty='dashed')
+            text(x=quantile(x,probs = 0.99,na.rm = T),y=quantile(y,probs = 0.99,na.rm = T),
+                 labels = paste('R=',round((cor(x,y,use ='na.or.complete')),2)),col='red' )
+            
+          }else{
+           plot(1,main='NA')
+          }
+        }))
+  title(site.vec[i])
+}
+
+
 dev.off()
